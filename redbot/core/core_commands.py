ea6cb1f3d80373348806021de5e3462a0da40e0b
Arnav singh <100131691+AffirmativeGuy@users.noreply.github.com>
2024-06-21 10:18:22 +0530

    Update core_commands.py

--- redbot/core/core_commands.py
+++ redbot/core/core_commands.py
@@ -556,7 +556,7 @@ class Core(commands.commands._RuleDropper, commands.Cog, CoreLogic):
         uptime = self.bot.uptime.replace(tzinfo=datetime.timezone.utc)
         uptime_str = humanize_timedelta(timedelta=delta) or _("Less than one second.")
         await ctx.send(
-            _("I have not since **{time_quantity} 😭!** (which is {timestamp})").format(
+            _("I have not slept since **{time_quantity} 😭!** (which is {timestamp})").format(
                 time_quantity=uptime_str, timestamp=discord.utils.format_dt(uptime, "f")
             )
         )
