7994d1f7d213312c3796da94ab6d8bb59e419064
Arnav singh <100131691+AffirmativeGuy@users.noreply.github.com>
2024-06-21 21:07:17 +0530

    Update __init__.py

--- redbot/cogs/cop/__init__.py
+++ redbot/cogs/cop/__init__.py
@@ -7,5 +7,7 @@ async def setup(bot):
     bot.get_command("info")
     bot.remove_command("info")
     bot.get_command("ping")
+    bot.get_command("inviteset")
+    bot.remove_command("inviteset")
     bot.remove_command("ping")
     await bot.add_cog(cop(bot))
